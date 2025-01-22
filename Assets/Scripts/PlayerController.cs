531bb3e7df79df21ec80c6b36935f6d862532a17
Butyricoil <butyricoil@gmail.com>
2025-01-22 15:32:12 +0400

    scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
-
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Joystick joystick;
+    public float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            rb.rotation = Quaternion.Lerp(rb.rotation, toRotation, Time.fixedDeltaTime * 10f);
+        }
     }
 }
